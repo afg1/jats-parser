f6a83ba6a909e014591c530baeec8172a96c7730
Andrew Green <agreen@ebi.ac.uk>
2023-08-21 11:17:42 +0100

    First attempt SLURM job

---
+++ slurm_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=convert_xml_2_parquet
+#SBATCH --array=0-981
+#SBATCH -p datamover
+#SBATCH --mem 4G
+#SBATCH --time=8:00:00
+
+
+cd /hps/nobackup/agb/rnacentral/agreen/ARISE/jats-parser/
+
+poetry run xml_2_parquet /nfs/ftp/public/databases/pmc/oa/ /hps/nobackup/agb/rnacentral/agreen/ARISE/jats-parser/oa_parquet --file_index $SLURM_ARRAY_TASK_ID
